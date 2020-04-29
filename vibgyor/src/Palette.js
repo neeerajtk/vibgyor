86dd61cd165103688a85c9b3e328112bb8edb942
neeerajtk <neerajtk560@gmail.com>
2020-04-30 01:22:16 +0530

    :fire: Slider styled

--- vibgyor/src/Palette.js
+++ vibgyor/src/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
     constructor(props){
@@ -25,11 +25,13 @@ class Palette extends Component {
         ))
         return (
             <div className="Palette">
+                <div className="slider">
                 <Slider defaultValue={level}
                  min={100} 
                  max={900}
                  step={100}
                  onAfterChange={this.changeLevel} />
+                 </div>
                 {/* navbar */}
                 <div className="Palette-colors">
                     {/* color boxes */}
