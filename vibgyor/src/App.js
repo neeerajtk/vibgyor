3c63feea1b220993835444beac40fa0543ecdd28
neeerajtk <neerajtk560@gmail.com>
2020-04-29 23:34:49 +0530

    :boom: added color helper to generate colors

--- vibgyor/src/App.js
+++ vibgyor/src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {generatePalette} from "./colorHelpers";
 
 
 class App extends Component {
   render(){
+    console.log(generatePalette(seedColors[4]));
+    
   return (
     <div>
         <Palette {...seedColors[5]}/>
