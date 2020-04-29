596e6584a095dc4d73ad36d3c7fc094b90783ca0
neeerajtk <neerajtk560@gmail.com>
2020-04-29 22:40:19 +0530

    :fire: animation styled

--- vibgyor/src/ColorBox.js
+++ vibgyor/src/ColorBox.js
@@ -22,7 +22,11 @@ class ColorBox extends Component {
                 <div
                  style={{background}} 
                  className={`copy-overlay ${copied && "show"}`}
-                 /> 
+                 />
+                 <div className={`copy-msg ${copied && "show"}`}>
+                     <h1>Copied !</h1>
+                     <p>{this.props.background}</p>
+                </div> 
                 <div className="copy-container">
                     <div className="box-content">
                         <span>{name}</span>
