ddcc585ed98b86b552d9a7d1965b5d2fe0f30eb9
neeerajtk <neerajtk560@gmail.com>
2020-04-30 01:05:13 +0530

    :boom: added color slider

--- vibgyor/src/Palette.js
+++ vibgyor/src/Palette.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            level:500
+        };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level){
+        this.setState({
+            level
+        });
+    }
     render() {
-        const colorBoxes = this.props.palette.colors[500].map(color=>(
+        const {colors} = this.props.palette;
+        const {level} = this.state;
+        const colorBoxes = colors[level].map(color=>(
             <ColorBox background={color.hex} name={color.name}/>
         ))
         return (
             <div className="Palette">
+                <Slider defaultValue={level}
+                 min={100} 
+                 max={900}
+                 step={100}
+                 onAfterChange={this.changeLevel} />
                 {/* navbar */}
                 <div className="Palette-colors">
                     {/* color boxes */}
