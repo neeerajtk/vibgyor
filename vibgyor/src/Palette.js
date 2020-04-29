c3ac83ded0fafecd885633088a9a8d9b08801631
neeerajtk <neerajtk560@gmail.com>
2020-04-29 20:19:08 +0530

    :boom: color boxes added

--- vibgyor/src/Palette.js
+++ vibgyor/src/Palette.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 
 class Palette extends Component {
     render() {
+        const colorBoxes = this.props.colors.map(color=>(
+            <ColorBox background={color.color} name={color.name}/>
+        ))
         return (
             <div className="Palette">
                 {/* navbar */}
                 <div className="Palette-colors">
                     {/* color boxes */}
+                    {colorBoxes}
                 </div>
                 {/* footer */}
             </div>
