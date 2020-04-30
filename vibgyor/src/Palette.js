a845d8cd0738c90fa69adafe5797a6b802ebcdc3
neeerajtk <neerajtk560@gmail.com>
2020-04-30 15:40:06 +0530

    :boom: palette footer added

--- vibgyor/src/Palette.js
+++ vibgyor/src/Palette.js
@@ -22,10 +22,10 @@ class Palette extends Component {
         })
     }
     render() {
-        const {colors} = this.props.palette;
+        const {colors, paletteName, emoji} = this.props.palette;
         const {level,format} = this.state;
         const colorBoxes = colors[level].map(color=>(
-            <ColorBox background={color[format]} name={color.name}/>
+            <ColorBox background={color[format]} name={color.name} key={color.id}/>
         ))
         return (
             <div className="Palette">
@@ -33,12 +33,12 @@ class Palette extends Component {
                level={level} 
                changeLevel={this.changeLevel} 
                handleChange={this.changeFormat} />
-                {/* navbar */}
-                <div className="Palette-colors">
-                    {/* color boxes */}
-                    {colorBoxes}
-                </div>
-                {/* footer */}
+
+                <div className="Palette-colors">{colorBoxes}</div>
+                <footer className="Palette-footer">
+                    {paletteName}
+                <span className="emoji">{emoji}</span>
+                </footer>
             </div>
         )
     }
