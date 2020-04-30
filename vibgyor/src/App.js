0a1e11818c42146129312404e6d7ccdfd10aa56f
neeerajtk <neerajtk560@gmail.com>
2020-04-30 23:47:34 +0530

    :fire: added single color palete and link

--- vibgyor/src/App.js
+++ vibgyor/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route,Switch} from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
 
@@ -24,7 +25,7 @@ class App extends Component {
           />
       )}
       />
-      <Route exact path="/palette/:paletteId/:colorId" render={()=><h1>Hello</h1>}/>
+      <Route exact path="/palette/:paletteId/:colorId" render={()=><SingleColorPalette/>}/>
     </Switch>
     
     // <div>
---
+++ vibgyor/src/SingleColorPalette.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+ class SingleColorPalette extends Component {
+    render() {
+        return (
+            <div>
+                <h1>single</h1>
+            </div>
+        )
+    }
+}
+export default SingleColorPalette;
