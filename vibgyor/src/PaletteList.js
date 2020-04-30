1e043c0557760692e48d09fb50f63d3dc46812de
neeerajtk <neerajtk560@gmail.com>
2020-04-30 20:12:04 +0530

    :boom: jss minipalete add

--- vibgyor/src/PaletteList.js
+++ vibgyor/src/PaletteList.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
 import {Link} from 'react-router-dom';
-
 class PaletteList extends Component {
     render() {
         const {palettes} = this.props;
         return (
             <div>
+                <MiniPalette/>
                 <h1><Link to="/">VIBGYOR</Link></h1>
                 {palettes.map(palette =>(
-                    <p>
-                    <Link to={`/palette/${palette.id}`}>
-                        {palette.paletteName}
-                    </Link>
-                    </p>
+                    <MiniPalette {...palette}/>
                 ))}
             </div>
         );
