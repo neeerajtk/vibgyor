2fdf2fcca3408b4646a7c2159e77882f76047f6e
neeerajtk <neerajtk560@gmail.com>
2020-04-30 23:06:42 +0530

    :boom: Added link to palette

--- vibgyor/src/MiniPalette.js
+++ vibgyor/src/MiniPalette.js
@@ -57,7 +57,7 @@ function MiniPalette(props){
     ));
 
     return(
-        <div className={classes.root}> 
+        <div className={classes.root} onClick={props.handleClick}> 
             <div className={classes.colors}>
                 {miniColorBoxes}
             </div>
