2b5b361be19d396e67eb2a6a30d473e1e9ddab90
neeerajtk <neerajtk560@gmail.com>
2020-04-30 15:58:34 +0530

    :boom: added react router

--- vibgyor/src/App.js
+++ vibgyor/src/App.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
+import {Route,Switch} from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
 
 
 class App extends Component {
-  render(){
-    
-    
+  render(){       
   return (
-    <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={()=><h1>Hello</h1>}/>
+      <Route 
+      exact 
+      path="/palette/:id" 
+      render={()=><h1>indiv</h1>}
+      />
+    </Switch>
+    
+    // <div>
+    //     <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 }
