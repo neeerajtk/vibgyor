4b6cbf497df96c218a3a8be337421997fed4105b
neeerajtk <neerajtk560@gmail.com>
2020-04-30 22:54:12 +0530

    :fire: minor style edits

--- vibgyor/src/PaletteList.js
+++ vibgyor/src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = {
     root:{
         backgroundColor: "blue",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "felx-start",
         justifyContent:"center"
