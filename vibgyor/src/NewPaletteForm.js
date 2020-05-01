788075fb7de19d98383513f69e5d464acdb4f11b
neeerajtk <neerajtk560@gmail.com>
2020-05-01 23:27:13 +0530

    :fire: chromepicker buttons added

--- vibgyor/src/NewPaletteForm.js
+++ vibgyor/src/NewPaletteForm.js
@@ -10,6 +10,7 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Button from "@material-ui/core/Button"
 import {ChromePicker} from "react-color";
 
 const drawerWidth = 400;
@@ -126,11 +127,22 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant="h4">Design Your palette</Typography>
+          <div>
+            <Button variant="contained" color="secondary">
+                Clear Palette
+            </Button>
+            <Button variant="contained" color="primary">
+                Random Color
+            </Button>
+          </div>
           <ChromePicker 
           color="purple"
           onChangeComplete={newColor=>console.log(newColor)}
            />
-
+        <Button variant="contained" color="primary">
+            Add Color
+        </Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
