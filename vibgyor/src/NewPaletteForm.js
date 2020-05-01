5e4a773bd4a266594eba1227634d3d739d36c9b4
neeerajtk <neerajtk560@gmail.com>
2020-05-02 00:21:47 +0530

    :fire: colorpicker connected to button

--- vibgyor/src/NewPaletteForm.js
+++ vibgyor/src/NewPaletteForm.js
@@ -77,9 +77,11 @@ class NewPaletteForm extends Component {
        super(props);
        this.state = {
            open: true,
-           currentColor: "teal"
+           currentColor: "teal",
+           colors: ["purple","#e15764"]
        };
        this.updateCurrentColor = this.updateCurrentColor.bind(this);
+       this.addNewColor=this.addNewColor.bind(this);
    }
     
 
@@ -94,6 +96,11 @@ class NewPaletteForm extends Component {
   updateCurrentColor(newColor){
       this.setState({currentColor: newColor.hex})
   }
+
+  addNewColor(){
+      this.setState({colors: [...this.state.colors, this.state.currentColor]});
+  }
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -149,7 +156,12 @@ class NewPaletteForm extends Component {
           color={this.state.currentColor}
           onChangeComplete={this.updateCurrentColor}
            />
-        <Button variant="contained" color="primary" style={{backgroundColor: this.state.currentColor}}>
+        <Button 
+        variant="contained" 
+        color="primary" 
+        style={{backgroundColor: this.state.currentColor}}
+        onClick={this.addNewColor}
+        >
             Add Color
         </Button>
         </Drawer>
@@ -159,7 +171,11 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          
+           <ul>
+               {this.state.colors.map(color =>(
+                   <li style={{backgroundColor: color}}>{color}</li>
+               ))}
+           </ul>
         </main>
       </div>
     );
