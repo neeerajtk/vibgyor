d19ef7a65b79b47f609159ca5fd0dc308a5c8f07
neeerajtk <neerajtk560@gmail.com>
2020-05-01 12:32:33 +0530

    :fire: threshold updated

--- vibgyor/src/ColorBox.js
+++ vibgyor/src/ColorBox.js
@@ -19,7 +19,7 @@ class ColorBox extends Component {
     const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
     const isDarkColor = chroma(background).luminance() <= 0.08;
-    const isLightColor = chroma(background).luminance() >= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className='ColorBox'>
@@ -29,13 +29,13 @@ class ColorBox extends Component {
           />
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>copied!</h1>
-            <p>{this.props.background}</p>
+            <p className={isLightColor&&"dark-text"}>{this.props.background}</p>
           </div>
           <div className='copy-container'>
             <div className='box-content'>
               <span className={isDarkColor&&"light-text" }>{name}</span>
             </div>
-            <button className='copy-button'>Copy</button>
+            <button className={`copy-button ${isLightColor&&"dark-text"}`} >Copy</button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
