10d9d0d2fdcc49187ebf2781c8bdfa5ba38b5b93
neeerajtk <neerajtk560@gmail.com>
2020-05-01 20:14:35 +0530

    :fire: navbar moved

--- vibgyor/src/Navbar.js
+++ vibgyor/src/Navbar.js
@@ -5,12 +5,60 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
-
-
+import {withStyles} from '@material-ui/styles';
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
+
+
+const styles = {
+    Navbar:{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        height: "6vh",
+    },
+    logo:{
+        marginRight: "15px",
+        padding: "0 13px",
+        fontSize: "22px",
+        backgroundColor: "#eceff1",
+        fontFamily: "Roboto",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        "& a":{
+            textDecoration: "none",
+            color: "black"
+        }
+    },     
+    slider: {
+        width: "340px",
+        margin: "0 10px",
+        display: "inline-block",
+        "& .rc-slider-track:" :{
+            backgroundColor: "transparent"
+        },
+        "& .rc-slider-rail":{
+            height:"8px"
+        },
+        "& .rc-slider-handle,.rc-slider-handle:active,.rc-slider-handle:focus,.rc-slider-handle:hover ":{
+            backgroundColor: "green",
+            outline: "none",
+            border: "2px solid green",
+            boxShadow: "none",
+            width: "13px",
+            height: "13px",
+            marginLeft: "7px",
+            marginTop: "-3px"
+        }
+    },
+    selectContainer:{
+        marginLeft: "auto",
+        marginRight: "1rem"
+    }
+}
+
 
 
 class Navbar extends Component {
@@ -34,17 +82,17 @@ class Navbar extends Component {
         this.setState({open:false})
     }
     render() {
-        const {level,changeLevel, showingAllColors}=this.props;
+        const {level,changeLevel, showingAllColors, classes}=this.props;
         const {format} = this.state;
         return (
-            <header className="Navbar"> 
-                <div className="logo">
+            <header className={classes.Navbar}> 
+                <div className={classes.logo}>
                     <Link to="/">VIBGYOR</Link>
                 </div>
                 {showingAllColors && ( 
-                <div className="slider-container">
+                <div>
                     <span>level : {level}</span>
-                    <div className="slider">
+                    <div className={classes.slider}>
                         <Slider defaultValue={level}
                         min={100} 
                         max={900}
@@ -53,7 +101,7 @@ class Navbar extends Component {
                     </div>
                 </div>
                 )}
-                <div className="select-container">
+                <div className={classes.selectContainer}>
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #ffffff </MenuItem>
                         <MenuItem value="rgb">RGB - rgb(255,255,255) </MenuItem>
@@ -83,4 +131,4 @@ class Navbar extends Component {
         )
     }
 }
-export default Navbar;
+export default withStyles(styles)(Navbar);
