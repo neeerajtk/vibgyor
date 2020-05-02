18dfeca411d4ea0521ce368b546c3b8baa2bc02c
neeerajtk <neerajtk560@gmail.com>
2020-05-02 23:55:34 +0530

    :fire: color box delete func added

--- vibgyor/src/NewPaletteForm.js
+++ vibgyor/src/NewPaletteForm.js
@@ -139,6 +139,13 @@ class NewPaletteForm extends Component {
       this.props.savePalette(newPalette);
       this.props.history.push("/");
   }
+
+  removeColor(colorName){
+      this.setState({
+          colors:this.state.colors.filter(color=>color.name !== colorName)
+      });
+  }
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -237,7 +244,7 @@ class NewPaletteForm extends Component {
         >
           <div className={classes.drawerHeader} />
           {this.state.colors.map(color => (
-            <DraggableColorBox color={color.color} name={color.name} />
+            <DraggableColorBox color={color.color} name={color.name} handleClick={()=>this.removeColor(color.name)} />
           ))}
         </main>
       </div>
