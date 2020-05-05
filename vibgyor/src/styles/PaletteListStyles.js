f04fe39a07351953786784e632e5781d1f60c4bd
neeerajtk <neerajtk560@gmail.com>
2020-05-05 20:40:07 +0530

    :fire: palettelist made respomsive

--- vibgyor/src/styles/PaletteListStyles.js
+++ vibgyor/src/styles/PaletteListStyles.js
@@ -1,35 +1,47 @@
-export default{
-    root:{
-        backgroundColor: "blue",
-        height: "100vh",
-        display: "flex",
-        alignItems: "felx-start",
-        justifyContent:"center"
+import sizes from "./sizes";
+export default {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%"
     },
-    container:{
-        width: "50%",
-        display: "flex",
-        alignItems:"flex-start",
-        flexDirection:"column",
-        flexWrap:"wrap",
-     
-    },
-    nav:{
-        display: "flex",
-        width:"100%",
-        justifyContent:"space-between",
-        alignItems:"center",
-        color:"white",
-        "& a": {
-            color:"white",
-            textDecoration:"none"
-        }
-    },
-    palettes:{
-        boxSizing: "border-box",
-        width:"100%",
-        display:"grid",
-        gridTemplateColumns: "repeat(3,30%)",
-        gridGap:"5%"
+    [sizes.down("xs")]: {
+      width: "75%"
     }
-} 
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "white",
+    "& a": {
+      color: "white"
+    }
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem"
+    }
+  }
+};
